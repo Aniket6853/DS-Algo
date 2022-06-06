d219f7f03154c5e42f600b205b09b8785ef03ec2
Aniket6853 <gholapaniket44@gmail.com>
2022-06-06 08:09:00 +0530

    initial commit

---
+++ reverseAnArray.cpp
@@ -0,0 +1,34 @@
+// Q1 Reverse the array
+#include<iostream>
+using namespace std;
+void ReverseArray(int arr[],int start, int end)
+{
+	while(start < end)
+	{
+		int temp = arr[start];
+		arr[start]=arr[end];
+		arr[end]=temp;
+		start++;
+		end--;
+	}
+}
+void PrintArray(int arr[], int size)
+{
+	int i;
+	for(i=0;i<size;i++)
+	{
+		cout<<arr[i];
+	}
+}
+int main()
+{
+	int arr[]={1,2,3,4,5};
+	int n = sizeof(arr)/sizeof(arr[0]);
+    
+	//Initial Array
+	PrintArray(arr,n);
+    
+	ReverseArray(arr,0,n-1);
+	//Final Array after Reversing
+	PrintArray(arr,n);
+}
